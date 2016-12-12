8a1be90a193adfaee62ee385d8522a43b7196053
System Administrator <kanaichenshikan@outlook.com>
2016-12-12 08:11:13 +0800

    construct LinkedList.js

--- core/Collection/List/LinkedList.js
+++ core/Collection/List/LinkedList.js
@@ -2,11 +2,13 @@ function LinkedList() {
 	// super constructor
 	List.call(this);
 
+	// variables
+	var mirror = this.toArray();
+
 	// methods declarations
 	this.addFirst = addFirst; this.offerFirst = addFirst;
 	this.addLast = addFirst; this.offer = addLast; this.offerLast = addLast;
-	this.element = element; this.peek = element;
-	this.getFirst = getFirst;
+	this.element = element; this.peek = element; this.getFirst = element;
 	this.getLast = getLast;
 	this.poll = poll;
 	this.pop = pop;
@@ -16,4 +18,47 @@ function LinkedList() {
 	this.descendingIterator = descendingIterator;
 
 	// methods implementations
+
+	function addFirst(element) {
+		mirror.reverse();
+		mirror.push(element);
+		mirror.reverse();
+	}
+
+	function addLast() {
+		mirror.push(element);
+	}
+
+	function element() {
+		return mirror[0];
+	}
+
+	function getLast() {
+		var last = mirror[mirror.length];
+	}
+
+	function poll() {
+		return mirror.shift();
+	}
+
+	function pop() {
+		return mirror.pop();
+	}
+
+	function push(element) {
+		mirror.push(element);
+	}
+
+	function removeFirst() {
+		mirror.shift();
+	}
+
+	function removeLast() {
+		mirror.pop();
+	}
+
+	function descendingIterator() {
+		
+	}
+
 }
